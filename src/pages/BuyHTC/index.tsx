3439ea1949713a23f976450eb7421e0f203484ae
thomasken93 <thomasken930603@gmail.com>
2025-01-25 14:29:59 -0500

    fix issue

--- src/pages/BuyHTC/index.tsx
+++ src/pages/BuyHTC/index.tsx
@@ -1,44 +1,43 @@
 import { Community, Content, Header, Rewards } from '@/components'
-import { UserSection } from '@/components/UserSection';
-import { ComingSoon } from '@/components/MusicPlayer';
-import { OverlayButton } from '@/components/OverlayButton';
-import BackLogo from '@/assets/images/Group 1000002443.png';
-import { Switch } from '@/components/Switch';
-import { useCallback, useState } from 'react';
-import { WithTon } from './WithTon';
-import { WithStars } from './WithStars';
+import { UserSection } from '@/components/UserSection'
+import { OverlayButton } from '@/components/OverlayButton'
+import BackLogo from '@/assets/images/Group 1000002443.png'
+import { Switch } from '@/components/Switch'
+import { useCallback, useState } from 'react'
+import { WithTon } from './WithTon'
+import { WithStars } from './WithStars'
 
 export const BuyHTC = () => {
-  const [tabOption, setTabOption] = useState(0);
+  const [tabOption, setTabOption] = useState(0)
 
-  const onTabChange = useCallback((option: any) => {
-    setTabOption(option)
-  }, [setTabOption])
+  const onTabChange = useCallback(
+    (option: any) => {
+      setTabOption(option)
+    },
+    [setTabOption],
+  )
 
   return (
     <div>
       <Header />
       <div className="mt-[51px]">
-        <UserSection/>
+        <UserSection />
       </div>
       <div className="mt-[22px] w-full text-center">
-        <p className='font-[800] text-[32px]'>Balance: 10 $HTC</p>
+        <p className="font-[800] text-[32px]">Balance: 10 $HTC</p>
       </div>
-      <div className='flex justify-center mt-3'>
+      <div className="flex justify-center mt-3">
         <Switch
-          width='294px'
-          options={[
-            { label: 'Buy with TON' },
-            { label: 'Buy with Stars' },
-          ]}
+          width="294px"
+          options={[{ label: 'Buy with TON' }, { label: 'Buy with Stars' }]}
           onOptionChange={onTabChange}
         />
       </div>
-      <div className='mt-[36px]'>
+      <div className="mt-[36px]">
         {tabOption == 0 ? <WithTon /> : <WithStars />}
       </div>
-      <div className='relative top-[-72px] flex justify-center items-center'>
-        <img src={BackLogo}/>
+      <div className="relative top-[-72px] flex justify-center items-center">
+        <img src={BackLogo} />
       </div>
     </div>
   )
